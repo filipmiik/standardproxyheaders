3393da5cf36046a0e86957fdf5e8951946531c5f
Filip Mík <filip@miik.cz>
2025-04-09 22:22:28 +0200

    Change package name

---
+++ plugin_test.go
@@ -0,0 +1,12 @@
+package traefik_standard_proxy_headers_test
+
+import (
+	_ "context"
+	_ "net/http/httptest"
+	"testing"
+
+	_ "github.com/filipmiik/traefik-standard-proxy-headers"
+)
+
+func TestPlugin(t *testing.T) {
+}
